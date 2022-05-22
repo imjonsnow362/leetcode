42bd815e527eac626fe429c509d33842bdb7d1d6
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-05-22 17:12:16 +0530

    Time: 1 ms (100.00%), Space: 55 MB (26.37%) - LeetHub

---
+++ 283-move-zeroes/283-move-zeroes.java
@@ -0,0 +1,29 @@
+/*mysolutionclass Solution {
+    public void moveZeroes(int[] nums) {
+        int j=1;
+        //j here is writepointer which will move 0 to the end of the array 
+        //i will be used to read the array and check if the element is equal to 0 or not 
+        for(int i=1; i<nums.length; i++){
+            if(nums[i] == 0){
+                nums[j] = nums[i];
+                j--;
+                //return index -- move element to the other side 
+            }
+        }
+    }
+}
+*/
+
+class Solution {
+   public void moveZeroes(int[] nums) {
+      int index = 0;
+      for(int i=0; i < nums.length; i++) {
+        if(nums[i] != 0) {
+          nums[index++] = nums[i];
+      }
+    }
+      for(int i=index; i < nums.length; i++) {
+        nums[i] = 0;
+      }
+   }
+}
