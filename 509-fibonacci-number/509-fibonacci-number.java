9a76371f4d3d0480deaf9ae5a5f6589d22ed369c
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-05-23 21:48:56 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (76.92%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int fib(int n) {
+        return nthFib(n, new HashMap<Integer, Integer>());
+    }
+
+    private int nthFib(int n, HashMap<Integer, Integer> memo){
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+        int currentKey = n;
+        if(memo.containsKey(currentKey))
+            return memo.get(currentKey);
+
+        int a = nthFib(n-1, memo);
+        int b = nthFib(n-2, memo);
+
+        memo.put(currentKey, a + b);
+        return a+b; 
+    }
+}
+/*
+    class Solution {
+    public int fib(int n) {
+        return nthFib(n);
+    }
+
+    private int nthFib(int n){
+        if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+
+
+        int a = nthFib(n-1);
+        int b = nthFib(n-2);
+
+        return a+b; 
+    }
+}
+*/
