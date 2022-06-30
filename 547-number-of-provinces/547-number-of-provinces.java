1bd5afe69144fcda7c783dcfe3c8910ebe7b6b6a
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-06-30 20:55:32 +0530

    Time: 2 ms (67.22%), Space: 53.7 MB (71.05%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.java
+++ 547-number-of-provinces/547-number-of-provinces.java
@@ -1,20 +1,25 @@
 class Solution{
    public int findCircleNum(int[][]graph){
+       //first lets find out the number of cities that means the number of vertices 
         int n = graph.length;
+       //this will store the number of provinces
         int answer = 0;
-        
+        //Initialise a visited array
         boolean[] visited = new boolean[n];
-       
+       //I want to run a DFS for all the vertices  
         for(int currentCity = 0; currentCity<n; currentCity++){
-            
+            //I will check whether my currentcity is visited or not 
             if(visited[currentCity]== false){
+                //starting point is n 
                 dfs(graph, currentCity, n, visited);
+                //when coming out of this dfs function we know we have got a connected component
                 answer += 1;
             }
         }
+       //finally after this for loop I will get all the connected components so I will return them
         return answer;
    }
-    
+    //Writing DFS function below
    private void dfs(int[][] graph,int currentCity, int n, boolean[] visited){
         
        if(visited[currentCity])
@@ -24,6 +29,7 @@ class Solution{
        int[] neighbours = graph[currentCity];
        
        for(int i=0; i<n; i++){
+           //If value is 1 I will simply run a DFS
             if(neighbours[i]==1)
                 dfs(graph,i,n,visited);
        }
