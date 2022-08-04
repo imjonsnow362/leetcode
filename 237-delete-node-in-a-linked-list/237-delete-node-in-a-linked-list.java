afbdaae3e1c3d60a618f57cf8c3da2fa44058354
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-08-04 17:37:54 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (14.32%) - LeetHub

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.java
@@ -8,7 +8,9 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
+        //I just need to put swap condition here 
         node.val = node.next.val;
+        //Now delete the next node (I will point the swapped node to the next of the node which got deleted)
         node.next = node.next.next;
     }
 }
