42210e7d4bfc3f6ab580033c9e39bea8e25bbd5a
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-08-07 13:40:01 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (64.30%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.java
@@ -10,16 +10,26 @@
  */
 class Solution {
     public ListNode middleNode(ListNode head) {
+        //To handle corner case when there is no node
         if(head == null)
             return null;
-        
+        //Initialise fastPtr and slowPointer pointing to head 
         ListNode fastPtr = head;
         ListNode slwPtr = head;
-        
-        while(fastPtr != null && fastPtr.next != null){
+        //we will put condition for fast pointer since fast pointer is 2times faster than slow pointer then we will put .next and .next.next 
+        while(fastPtr.next != null && fastPtr.next.next != null){
+            //Once the while condition satisifies I will update fp and sp to these
+            //fastPointer moves 2x faster than slowPointer so we put those conditions 
+            //fp 2 and sp 1
             fastPtr = fastPtr.next.next;
             slwPtr = slwPtr.next;
         }
+        //we need to make corner case for even length ll 
+        //if fp.next is not null it indicates it is even
+        if(fastPtr.next != null)
+            //this is the middle of the ll  
+            return slwPtr.next;
+        
         return slwPtr;
     }
 }
