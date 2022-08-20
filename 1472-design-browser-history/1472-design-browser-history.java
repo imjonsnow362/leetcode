4bae6e1c1bd88b5349502ccde67ff9c1cfeb2560
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-08-20 15:47:19 +0530

    Time: 109 ms (53.55%), Space: 81.5 MB (81.52%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -27,8 +27,10 @@ class BrowserHistory {
         DLLNode newNode = new DLLNode(url);
         //Once I have made the new node I need to add it to the ll (aka attach it to the nodes)
         //increment iterator by 1 and point next of iterator to the newNode 
+        //line 30 and 31 is to put a doubly link ←→ like this between two nodes
         iterator.next = newNode;
         newNode.prev = iterator;
+        //after putting link then point iterator to the latest node
         iterator = newNode;
         return;
     }
@@ -38,6 +40,7 @@ class BrowserHistory {
         while(iterator.prev != null && steps > 0){
             //moving back
             iterator = iterator.prev;
+            //this is just decrementing the count of steps till it reaches 0 thats why we have put it in while loop (steps is basically k here k can be the number which is given in the []square bracket)
             steps -= 1;
         }
         //after this while loop only one condition will be valid iterator.prev != null or steps > 0 so return the url of the node which iterator is pointing to
@@ -49,6 +52,7 @@ class BrowserHistory {
         while(iterator.next != null && steps > 0){
             //moving forward
             iterator = iterator.next;
+            //this is just decrementing the count of steps till it reaches 0 thats why we have put it in while loop (steps is basically k here k can be the number which is given in the []square bracket)
             steps -= 1;
         }
         //after this while loop only one condition will be valid iterator.next != null or steps > 0 so return the url of the node which iterator is pointing to
