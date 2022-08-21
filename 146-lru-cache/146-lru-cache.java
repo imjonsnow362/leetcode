bbf5d99166d3de52c42d80c3b0a999886a4a68c3
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-08-21 18:13:05 +0530

    Time: 84 ms (64.03%), Space: 132.8 MB (31.48%) - LeetHub

--- 146-lru-cache/146-lru-cache.java
+++ 146-lru-cache/146-lru-cache.java
@@ -20,63 +20,74 @@ class LRUCache {
     Map<Integer, CdllNode> cache;
     
     public LRUCache(int capacity) {
+        //-①→←-① to make a doubly link between -1,-1 we use this code line 24,25 (check page 79)
         head = new CdllNode(-1, -1);
         tail = new CdllNode(-1, -1);
-
+        //to create a circular link between the two nodes and normal doubly link between two nodes 
         head.next = tail;
         tail.prev = head;
         head.prev = tail;
         tail.next = head;
-
+        
         cache = new HashMap<>();
         maxSize = capacity;
     }
     
     public int get(int key) {
-        
+        //First check whether it is present in the Map or not (the map name is cache)
         if(cache.containsKey(key)){
+            //I will get the reference of that node using map.get(key)
             CdllNode currentNode = cache.get(key);
+            //Once I have the node I need to delete it from that position and add it to the tail for line 42,43
             deleteNode(currentNode);
             addNodeAtTail(currentNode);
+            //return the value of the node using key (in the map)
             return currentNode.val;
         }else{
+            //return null or -1 because the node is not present in the map (it is given in description to return -1)
             return -1;
         }
     }
     
     public void put(int key, int value) {
-        
+        //if we have reached maximum capacity 
         if(currentSize == maxSize){
-            
+            //again we have to check the same thing from line 75 
             if(cache.containsKey(key)){
                 CdllNode currentNode = cache.get(key);
                 deleteNode(currentNode);
                 currentSize -= 1;
                 cache.remove(key);
                 
+                // CdllNode newNode = new CdllNode(key, value);
+                // addNodeAtTail(newNode);
+                // cache.put(key, newNode);
+                // currentSize += 1;
+                //if the key is not present in the map I need to delete a node from the front because the capacity is maxed out so we delete the LRU node
             }else{
                 CdllNode current = head.next;
                 deleteNode(current);
                 cache.remove(current.key);
                 currentSize -= 1;
             }
-            
+            //create a node and put it 
             CdllNode newNode = new CdllNode(key, value);
             addNodeAtTail(newNode);
             cache.put(key, newNode);
             currentSize += 1;
-            
+        //if it is less then capacity 
         }else{
-            
+            //if the key is already present that means the app is already open so we will delete it from that position and put it at tail  
             if(cache.containsKey(key)){
                 CdllNode currentNode = cache.get(key);
                 deleteNode(currentNode);
                 currentSize -= 1;
                 cache.remove(key);
             }
-            
+            //if the key is new then we have to add it 
             CdllNode newNode = new CdllNode(key, value);
             addNodeAtTail(newNode);
+            //add key to hashmap
             cache.put(key, newNode);
             currentSize += 1;
         }
