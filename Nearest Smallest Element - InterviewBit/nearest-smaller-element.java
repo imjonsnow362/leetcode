f2bc9ecafd436da8fcf14c39983e241ca4fe101c
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-08-31 16:12:38 +0530

    Create nearest-smaller-element.java

---
+++ Nearest Smallest Element - InterviewBit/nearest-smaller-element.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public ArrayList<Integer> prevSmaller(ArrayList<Integer> A) {
+        //I want to return the answer in form of ArrayList so 
+        List<Integer> answer = new ArrayList<>();
+        //Initialise stack 
+        Stack<Integer> stack = new Stack<>();
+        
+        for(int currentVal:A){
+            
+        }
+        
+    }
+}
