0765782aad447d58c933affb3cd72d0cbede7908
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-08-31 16:16:02 +0530

    Update nearest-smaller-element.java

--- Nearest Smallest Element - InterviewBit/nearest-smaller-element.java
+++ Nearest Smallest Element - InterviewBit/nearest-smaller-element.java
@@ -1,13 +1,21 @@
 public class Solution {
     public ArrayList<Integer> prevSmaller(ArrayList<Integer> A) {
         //I want to return the answer in form of ArrayList so 
-        List<Integer> answer = new ArrayList<>();
+        ArrayList<Integer> answer = new ArrayList<>();
         //Initialise stack 
         Stack<Integer> stack = new Stack<>();
-        
+        //Iterate through the list 
         for(int currentVal:A){
             
+            while(!stack.isEmpty() && stack.peek() >= currentVal)
+                stack.pop();
+                
+            //there are two cases if the stack is empty we will put -1 otherwise we will put that element on top of the stack 
+            answer.add(stack.isEmpty() ? -1 : stack.peek());
+            stack.push(currentVal);
         }
+        
+        return answer;
         
     }
 }
