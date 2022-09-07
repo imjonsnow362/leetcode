66846e244c2dda14b5dd597369c591b138f70200
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-09-07 20:43:01 +0530

    Time: 0 ms (100.00%), Space: 45.3 MB (96.96%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,39 +1,68 @@
 class Solution {
-    
     public int[] searchRange(int[] nums, int target) {
-        int[] ans = {-1, -1};
-        // check for first occurrence if target first
-        ans[0] = search(nums, target, true);
-        if (ans[0] != -1) {
-            ans[1] = search(nums, target, false);
-        }
-        return ans;
+        //put corner case since we want 2 positions so we will check if there are any elements present in the array 
+        if(nums.length < 1)
+            //if there are no elements present then just return -1,-1
+            return new int[]{-1, -1};
+        
+        int[] answer = {-1, -1};
+        //code function for first position 
+        answer[0] = findFirstPosition(nums, target);
+        answer[1] = findLastPosition(nums, target);
+        
+        return answer;
     }
-
-    // this function just returns the index value of target
-    int search(int[] nums, int target, boolean findStartIndex) {
-        int ans = -1;
+    private int findFirstPosition(int[] nums, int target){
+        //initialise answer index with -1 initially 
+        int answerIndex = -1;
+        //initialise start and end
         int start = 0;
-        int end = nums.length - 1;
-        while(start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
-            int mid = start + (end - start) / 2;
-
-            if (target < nums[mid]) {
-                end = mid - 1;
-            } else if (target > nums[mid]) {
+        int end = nums.length-1;
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            //comparison part 
+            if(nums[mid]==target){
+                //if the mid is equal to target I will update my answer index to mid 
+                answerIndex = mid;
+                //if I get mid is equal to target I continue my binary search in left part so 
+                //update end to mid -1
+                end = mid-1;
+            }
+            else if(nums[mid] > target){
+                end = mid -1;
+            }else{
                 start = mid + 1;
-            } else {
-                // potential ans found
-                ans = mid;
-                if (findStartIndex) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
             }
         }
-        return ans;
+        return answerIndex;
+    }
+    
+    private int findLastPosition(int[] nums, int target){
+        //initialise answer index with -1 initially 
+        int answerIndex = -1;
+        //initialise start and end
+        int start = 0;
+        int end = nums.length-1;
+        
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            //comparison part 
+            if(nums[mid]==target){
+                //if the mid is equal to target I will update my answer index to mid 
+                answerIndex = mid;
+                //if I get mid is equal to target I continue my binary search in right part so 
+                //update start to mid +1
+                start = mid+1;
+            }
+            else if(nums[mid] > target){
+                end = mid - 1;
+            }else{
+                start = mid + 1;
+            }
+        }
+        return answerIndex;
     }
 }
