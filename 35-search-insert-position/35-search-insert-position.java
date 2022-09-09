d7095a94bd7add27360e46098a25ff327f27464b
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-09-09 13:21:51 +0530

    Time: 0 ms (100.00%), Space: 43.5 MB (52.10%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.java
+++ 35-search-insert-position/35-search-insert-position.java
@@ -7,17 +7,22 @@ class Solution {
         
         while(start <= end){
             int mid = start + (end-start)/2;
-            
+            //return the index if the target is found(from description)
             if(nums[mid] == target)
                 return mid;
-            
+            //if the mid is smaller than target 
             if(nums[mid] < target){
                 start = mid +1;
         }else{
+                //if nums[mid] > target 
+                //then it is possible nums[mid] is the correctIndex
             correctIndex = mid;
+                //and move to the left division 
             end = mid -1;
         }
     }
+        //covering corner case
+        //check page 110 for corner case explanation
     if(correctIndex==-1)
         return nums.length;
     return correctIndex;
