3e02b8bda79707880ae7f24ac3a97814551676af
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-09-10 15:57:31 +0530

    Time: 1 ms (76.31%), Space: 42.1 MB (81.33%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            
+            if(nums[mid]==target)
+                return mid;
+                
+            //if the below condition is true that means the left half is sorted 
+            if(nums[mid] >= nums[start]){
+                //Now we need to check whether the element is present in the left half or not
+                //If the below condition is the case then we go to left division
+                if(nums[start] <= target && nums[mid] > target)
+                    end = mid - 1;
+                else
+                    //otherwise move in the right half 
+                    start = mid + 1;
+            }else{
+                if(nums[mid] < target && nums[end] >= target)
+                start = mid + 1;   
+                
+                else 
+                    end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
