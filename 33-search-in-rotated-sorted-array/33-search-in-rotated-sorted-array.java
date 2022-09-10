bed48e6e2f9c17aa816147011263902d8aaa0ac9
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-09-10 16:08:19 +0530

    Time: 1 ms (76.31%), Space: 43 MB (19.02%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.java
@@ -11,12 +11,14 @@ class Solution {
             //if the below condition is true that means the left half is sorted 
             if(nums[mid] >= nums[start]){
                 //Now we need to check whether the element is present in the left half or not
+                //This will compare the smallest element < target < highest element
                 //If the below condition is the case then we go to left division
                 if(nums[start] <= target && nums[mid] > target)
                     end = mid - 1;
                 else
                     //otherwise move in the right half 
                     start = mid + 1;
+                //check whether right half is sorted or not
             }else{
                 if(nums[mid] < target && nums[end] >= target)
                 start = mid + 1;   
