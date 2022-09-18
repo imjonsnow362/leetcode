bd01c2c0fdefb5e31b2cc83e608e5d17c06516a8
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-09-18 16:55:16 +0530

    Time: 1 ms (80.33%), Space: 50.9 MB (68.44%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,13 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int sumArr=0;
-        
+        //This will remove sum of all the elements present in the array except the missing number 
         for(int i=0; i<nums.length; i++){
             sumArr+=nums[i];
         }
-        
+        //1....n so we need to define n as length of array / last element in array 
         int n = nums.length;
-        
+        //formula 
         int expectedSum = (n*(n+1))/2;
         //we have got expected sum and array sum we just need to subtrract them 
         int requirednumber = expectedSum - sumArr;
