a4aaaad41ccfecf79b5c9d8f242efbc1caadeebf
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-09-19 22:44:12 +0530

    Time: 3 ms (46.61%), Space: 55 MB (27.22%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -19,7 +19,8 @@ class Solution {
       int j = 0;
       for(int i=0; i < nums.length; i++) {
         if(nums[i] != 0) {
-          nums[j++] = nums[i];
+          nums[j] = nums[i];
+            j++;
       }
     }
       for(int i=j; i < nums.length; i++) {
