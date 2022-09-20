3fb9b68c3e0dca0faee89d9ddbad7cd530038cce
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-09-20 12:32:51 +0530

    Time: 65 ms (23.70%), Space: 68.5 MB (14.83%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,20 +1,33 @@
 class Solution {
 
     public int subarraySum(int[] nums, int k) {
+        //Initialise answer variable here 
         int answer = 0;
+        //Initialise prefixSum variable here 
         int prefixSum = 0;
-
+        
+        //Initialise HashMap here 
         HashMap<Integer, Integer> memo = new HashMap<>();
+        //create an entry in the map 
         memo.put(prefixSum, 1);
-
+        //Iterate over nums array 
         for (int currentVal : nums) {
+            //We will add this to the prefix sum (What we are doing in step 3 of procedure on page 143) 
+            
             prefixSum += currentVal;
-
-            if (memo.containsKey(prefixSum - k)) answer += memo.get(prefixSum - k);
-
-            if (memo.containsKey(prefixSum)) memo.put(prefixSum, memo.get(prefixSum) + 1); else memo.put(prefixSum, 1);
+            //Check if prefix sum - k is present in the map or not
+            if (memo.containsKey(prefixSum - k)) 
+                //If the key is already present in the map we will increment answer by 1.
+                answer += memo.get(prefixSum - k);
+            //Otherwise come outside the previous if and check whether prefixSum is present in the map or not 
+            if (memo.containsKey(prefixSum)) 
+                //If the ps and key is already present I will not make a new entry I will increase frequency by 1 
+                memo.put(prefixSum, memo.get(prefixSum) + 1); 
+            //Else part the key is not present in the memo that means the pS is coming first time 
+            else 
+                memo.put(prefixSum, 1);
         }
-
+        //Outside return the answer variable 
         return answer;
     }
 }
