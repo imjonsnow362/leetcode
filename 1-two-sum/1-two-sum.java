5132d02d6c71ab720bd873369fd63a4f2e3351f3
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-09-22 11:21:23 +0530

    Time: 5 ms (69.56%), Space: 46.1 MB (21.28%) - LeetHub

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        //Initialise a HashMap
         HashMap<Integer, Integer> memo = new HashMap<>();
-        
+        //iterate through the array 
         for(int i = 0; i < nums.length; i++){
+            //currentVal is equal to the value present while iterating
             int currentVal = nums[i];
-            int comp = target - currentVal;
-            
-            if(memo.containsKey(comp)){
-                int arr[] = {i, memo.get(comp)};
-                return arr;
+            //using formula a+b=target (Check Page 147 for explanation)
+            //complement is a , currentVal is b
+            int complement = target - currentVal;
+            //Check if the complement is already present in the map or not
+            if(memo.containsKey(complement)){
+                //we need to return the answer in integer (we need to return the value at which the complement is coming and the currentIndex)
+                return new int[]{memo.get(complement),i};
             }            
-            
+            //other wise if we havent encountered the value before we will add the value in the map 
             memo.put(currentVal, i);
-            
         }
         return new int[2];
     }
