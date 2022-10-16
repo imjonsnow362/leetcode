f0570ecdecc6734e124c8bab26583c081613b9aa
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-10-16 18:56:49 +0530

    Time: 1 ms (92.59%), Space: 41.7 MB (78.00%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,7 +4,11 @@ class Solution {
         if(strs.length==0) return "";
         //We will create a String named prefix and store the first string of the String array in this
         String prefix = strs[0];
+        //We will iterate through the array till length of the array
         for(int i = 0; i<strs.length; i++){
+            //While loop so whatever string is present at ith index we will check its index using indexOf prefix and we know that prefix starts from index 0 so 
+            //If it is not equal to 0 so we will modify it 
+            //Then we remove the substring and check which is the longest using start 0 and length of prefix - 1
             while(strs[i].indexOf(prefix)!=0){
                 prefix = prefix.substring(0, prefix.length() - 1);
                 if(prefix.isEmpty()) return "";
