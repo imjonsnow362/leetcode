4ef7ea70c98c7b5649caaac33a8f47a3b1df8cc1
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-10-16 19:57:44 +0530

    Time: 1 ms (92.59%), Space: 42.4 MB (31.74%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -7,9 +7,9 @@ class Solution {
         String prefix = strs[0];
         //We will iterate through the array till length of the array
         for(int i = 0; i<strs.length; i++){
-            //While loop so whatever string is present at ith index we will check its index using indexOf prefix and we know that prefix starts from index 0 so 
-            //If it is not equal to 0 so we will modify it 
-            //Then we remove the substring and check which is the longest using start 0 and length of prefix - 1
+            //While loop so whatever string is present at ith index we check it with prefix if the prefix is not matching we will remove letter from the end of the prefix using substring(0,n-1) method
+            //We will keep on removing characters till the prefix and ith string match 
+            //e.g. prefix = flower and array = ["flow"] so flower and flow dont match so we keep on reducing 1 character from the end flower̶ then again while loop is satisfying so again reduce 1 more char this time we get flowe̶r̶ now prefix and ith string are matching so we can go ahead to other words in the array to check whether they match the prefix or not and perform reduction if needed.
             while(strs[i].indexOf(prefix)!=0){
                 prefix = prefix.substring(0, prefix.length() - 1);
                 //if(prefix.isEmpty()) return "";
