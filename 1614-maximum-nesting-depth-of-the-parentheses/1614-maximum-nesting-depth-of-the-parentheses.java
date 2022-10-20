5007707ddedaac0fc9c407b38416fe1c0cfa451c
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-10-20 22:50:26 +0530

    Time: 2 ms (34.98%), Space: 42.1 MB (49.11%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.java
@@ -1,9 +1,13 @@
 class Solution {
     public int maxDepth(String s) {
+        //We will keep one count variable for opening and closing bracket 
+        //Depth will store maximum value of open 
         int depth = 0, open = 0;
         for(char c: s.toCharArray()){
+            //Increase count of open if we get opening bracket
             if(c == '(')
                 open++;
+            //Decrease count of open if we get closing bracket
             if(c == ')')
                 open--;
             depth = Math.max(depth, open);
