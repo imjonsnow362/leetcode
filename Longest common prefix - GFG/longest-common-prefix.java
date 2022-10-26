e6585fdb67d6d44ac2398422909d95c613ff9f08
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-10-26 18:03:17 +0530

    Added solution - LeetHub

--- Longest common prefix - GFG/longest-common-prefix.java
+++ Longest common prefix - GFG/longest-common-prefix.java
@@ -29,12 +29,12 @@ class GfG {
 class Solution {
     public int[] longestCommonPrefix(String s1, String s2){
         //code here
-        int[] ans = {-1, -1};
+        int[] ans = {-1,-1};
         for(int i=0; i<s1.length(); i++){
             if(s2.contains(s1.substring(0,i))){
-            ans[0] = 0;
-            ans[1] = i-1;
-        }
+                ans[0]=0;
+                ans[1]=i-1;
+            }
         }
         return ans;
     }
