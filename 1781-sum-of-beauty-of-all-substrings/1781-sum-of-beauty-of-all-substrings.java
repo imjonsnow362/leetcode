04a799b4ef0bb751d7a97c82afdbb81758a82c01
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-10-31 17:03:06 +0530

    Time: 116 ms (51.51%), Space: 43 MB (61.75%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.java
@@ -1,41 +1,28 @@
 class Solution {
-    private int getMinCount(int[] charCount) {
-        int min = Integer.MAX_VALUE;
-		
-        for (int i = 0; i < charCount.length; i++) {
-            if (charCount[i] != 0) {
-                min = Math.min(min, charCount[i]);
-            }
-        }
-		
-        return min;
-    }
-    
-    private int getMaxCount(int[] charCount) {
-        int max = 0;
-		
-        for (int i = 0; i < charCount.length; i++) {
-            max = Math.max(max, charCount[i]);
-        }
-		
-        return max;
-    }
-    
     public int beautySum(String s) {
-        int sum = 0;
-        
-        for (int i = 0; i < s.length(); ++i) {
-            int[] charCount = new int[26]; // initialize charCount to all 0
-            
-            for (int j = i; j < s.length(); ++j) {
-                ++charCount[s.charAt(j) - 'a'];
-
-				// get beauty of substring from i to j
-				int beauty = getMaxCount(charCount) - getMinCount(charCount);
-                sum += beauty;
+    int sum=0;
+    for(int i=0;i<s.length();i++)
+    {
+        int freq[]=new  int[26];
+        for(int j=i;j<s.length();j++)
+        {
+            char temp=s.charAt(j);
+            freq[temp-'a']++;
+            int min=Integer.MAX_VALUE;
+            int max=Integer.MIN_VALUE;
+            for(int val:freq)
+            {
+                if(val!=0)
+                {
+                    min=Integer.min(min,val);
+                    max=Integer.max(max,val);
+                }
             }
+            
+            if(max-min!=0)
+                sum+=max-min;
         }
-        
-        return sum;
     }
+    return sum;
+}
 }
