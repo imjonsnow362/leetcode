131e31e43ae4b539b205e59ae706dc2ea0e8b887
Akshat Bhatawdekar <65452407+imjonsnow362@users.noreply.github.com>
2022-11-13 17:23:22 +0530

    Time: 71 ms (20.63%), Space: 62.7 MB (35.87%) - LeetHub

---
+++ 0974-subarray-sums-divisible-by-k/0974-subarray-sums-divisible-by-k.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int subarraysDivByK(int[] nums, int k) {
+        int answer = 0;
+        int prefixSum = 0;
+        
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        //to handle corner cases I will create an entry for pS 0
+        memo.put(prefixSum, 1);
+        
+        //startr iterating over nums array 
+        for(int currentVal: nums){
+            prefixSum += currentVal;
+            //we need to handle cases for negative value (we need to make them positive)
+            int currentRem = ((prefixSum % k)+k)%k;
+            
+            if(memo.containsKey(currentRem)){
+                answer += memo.get(currentRem);
+                memo.put(currentRem, memo.get(currentRem)+1);
+            }else{
+                memo.put(currentRem, 1);
+            }
+        }
+        return answer;
+    }
+}
